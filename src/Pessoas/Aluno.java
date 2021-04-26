dfbfda0b498968621c761ee775c1275ac31904f9
Victor Kist <victoraugustokist@gmail.com>
2021-04-26 15:19:26 -0300

    Update Aluno.java

--- src/Pessoas/Aluno.java
+++ src/Pessoas/Aluno.java
@@ -14,11 +14,15 @@ public class Aluno extends Pessoa{
     public float media() {
         return this._media;
     }
-
+    //tentar remover \/ e/ou usar o metodo de baixo
     public String nome() {
         return this.nome;
     }
-
+    /*
+    public void nome() {
+        return this.nome;
+    }
+    */
     public void finalizarAno(float nota1, float nota2, float nota3) {
         float media = (nota1+nota2+nota3) / 3;
         this._media = media;
